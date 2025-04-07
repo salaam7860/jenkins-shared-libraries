9d54b5d841355004c7aaddd13815f90eff78610d
saad zaheer <22400549+salaam7860@users.noreply.github.com>
2025-04-07 12:10:16 +0500

    Create docker_build.groovy

---
+++ vars/docker_build.groovy
@@ -0,0 +1,11 @@
+def call(String ProjectName, String ImageTag, String DockerHubUser) {
+  def imageName = ${DockerHubUser}/${ProjectName}:${ImageTag}
+
+  def exitStatus = sh(script: "docker build -t ${imageName} .", returnStatus: true)
+
+  if (exitStatus != 0) {
+    echo "ERROR: Docker build failed with exit status ${exitStatus}."
+    }else {
+     echo "Docker build successful"
+    } 
+}
