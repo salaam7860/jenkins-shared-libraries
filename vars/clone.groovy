9cf8de899a0506a2bc778035944c27527051c703
saad zaheer <22400549+salaam7860@users.noreply.github.com>
2025-04-07 11:39:05 +0500

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,21 @@
+def call(String url, String branch) {
+  if (!url || !branch) {
+    sh "echo ERROR: +++++++++++++++++++ URL AND BRANCH ARE REQUIRED +++++++++++++++++++++++"
+    return
+  }
+
+  def validBranch = 'main' 
+  if (branch != validBranch) {
+    sh "echo ERROR: INVALID BRANCH"
+    return
+  }
+
+  try {
+    echo 'This is coping the code.'
+    git url: url, branch: branch, credentialsId: 'your-credentials-id'
+    echo "The code has been successfully cloned"
+  } catch (Exception e) {
+    sh "echo ERROR: Failed to clone repository - $(e.getMessage()}"
+  }
+      
+}
