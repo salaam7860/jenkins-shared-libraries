6978cfefcfd206c80c571a5792c9ae6a25b6a331
saad zaheer <22400549+salaam7860@users.noreply.github.com>
2025-04-07 12:36:29 +0500

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser) {
-  def imageName = ${DockerHubUser}/${ProjectName}:${ImageTag}
+  def imageName = "${DockerHubUser}/${ProjectName}:${ImageTag}"
 
   def exitStatus = sh(script: "docker build -t ${imageName} .", returnStatus: true)
 
