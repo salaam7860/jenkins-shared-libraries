04bdf029618be0b7127bfbf426f879ade01b8cc1
saad zaheer <22400549+salaam7860@users.noreply.github.com>
2025-04-07 12:25:16 +0500

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,15 @@
+def call(String projectName, String imageTag) {
+  withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]) {
+    try {
+      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+      sh "docker image tag notes-app:latest ${dockerHubUser}/${projectName}:${imageTag}"
+      sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
+    } catch (Exception e) {
+      echo "ERROR: Docker operation failed - ${e.getMessage()}"
+    } finally {
+      sh "docker logout"
+    }
+    
+  }
+  
+}
